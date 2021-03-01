ef8b8404d1fabc6f51cc898fb4c2b4c39e8ab9bb
Roonsine <tcarron@teksavvy.com>
2021-02-28 21:22:24 -0500

    Various Minor Changes

--- LeaveManagement/Models/LeaveTypeViewModel.cs
+++ LeaveManagement/Models/LeaveTypeViewModel.cs
@@ -11,6 +11,12 @@ namespace LeaveManagement.Models
         public int Id { get; set; }
         [Required]
         public string Name { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Required]
+        [Display(Name="Default Number of Days")]
+        [Range(1,105, ErrorMessage ="Please Enter A Valid Number")]
+        public int DefaultDays { get; set; }
         [Display(Name="Date Created")]
         public DateTime? DateCreated { get; set; }
     }
